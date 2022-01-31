caf78aa00d58b4c736eb410eeebe778162975ed0
richard <richard@nextmovesoftware.com>
2022-01-31 15:06:45 +0000

    first pass of Lomapper.

--- openfe/setup/__init__.py
+++ openfe/setup/__init__.py
@@ -1,3 +1,6 @@
 from .atommapping import AtomMapping
 from .alchemical_molecule import AlchemicalMolecule
+from .atommapper import AtomMapper
 from .network import Network
+
+from .lomap_mapper import LomapAtomMapper
